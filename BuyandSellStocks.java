634377be8f4a9413b977842846f2a9acc3956240
Taha <116361491+dexter-ifti@users.noreply.github.com>
2023-01-18 22:16:05 +0530

    Add files via upload

---
+++ BuyandSellStocks.java
@@ -0,0 +1,20 @@
+public class BuyandSellStocks {
+    public static int buyAndSellStocks(int prices[]){
+        int buyPrice = Integer.MAX_VALUE ;
+        int maxProfit = 0 ;
+        
+        for(int i = 0 ; i < prices.length; i++){
+            if(buyPrice < prices[i]){ // profit
+                int profit = prices[i] - buyPrice ;//today's profit
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i] ;
+            }   
+        }
+        return maxProfit ;
+    } 
+    public static void main(String[] args) {
+        int prices [] = {7, 1, 5, 3, 6, 4};
+        System.out.println(buyAndSellStocks(prices));
+    }
+}
