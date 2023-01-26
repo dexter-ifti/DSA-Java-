2df9a357ad9d73c14c2875f80ed3275cca4b7297
Taha <116361491+dexter-ifti@users.noreply.github.com>
2023-01-26 13:06:44 +0530

    Update BitOperations.java

--- Bit Manipulation/BitOperations.java
+++ Bit Manipulation/BitOperations.java
@@ -8,6 +8,12 @@ public class BitOperations {
             // System.out.println(setBit(10, 2));
             // System.out.println(clearBit(10, 2));
             //System.out.println(updateBit(10, 2, 1));
+            // System.out.println(clearIBit(15, 2));
+            // System.out.println(clearBitsInRange(10, 2, 4));
+            // System.out.println(check_Power_2(8));
+            // System.out.println(count_set_bits(10));
+            // System.out.println(fast_expo(5, 4));
+            
     } 
 
    public static void even_odd(int n) {
@@ -32,7 +38,7 @@ public class BitOperations {
         int bitMask = 1 << i ;
         return n | bitMask ;
     }    
-    
+   
     public static int clearBit(int n , int i) {
         int bitMask = ~(1 << i) ;
         return n & bitMask ;
@@ -47,5 +53,52 @@ public class BitOperations {
         n = clearBit(n, i);
         int BitMask = newBit << i ;
         return n | BitMask ;
+    } 
+   
+    public static int clearIBit(int n , int i) {
+        int bitMask = (~0) << i ;
+        return n & bitMask ;
+    }
+
+    public static int clearBitsInRange(int n, int i, int j) {
+        int a = ((~0) << (j+1));
+        int b = (1 << i) - 1 ;
+        int bitMask = a | b ;
+        return n & bitMask ;
+    }
+    
+    public static boolean check_Power_2(int n) {
+        // int bitMask = n & (n-1) ;
+        // if(bitMask == 0){
+        //     System.out.println(n+" is Power of 2");
+        //     return true ;
+        // }else {
+        //     System.out.println(n+" is not Power of 2");
+        //     return false;
+        // }
+        return (n & (n-1)) == 0 ;
+    }    
+    
+    public static int count_set_bits(int n) {
+        int count = 0 ;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++ ;
+            }
+            n = n >> 1 ;
+        }
+        return count;
+    }
+    public static int fast_expo(int a, int n) {
+        int ans = 1;
+        
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans *= a ;
+            }
+            a *= a ;
+            n = n >> 1 ;
+        }
+        return ans ;
     }    
 }
