2102330c8c9faa10422baafe2e534504fc878664
Taha <tiftikhar@student.iul.ac.in>
2023-02-12 18:41:07 +0530

    Create Rate_In_Maze.java

---
+++ 10_Backtracking/Rate_In_Maze.java
@@ -0,0 +1,73 @@
+public class Rate_In_Maze
+{
+  public static void main(String args[]){
+     int maze[][] = { { 1, 0, 0, 0 },
+                    { 1, 1, 0, 1 },
+                    { 0, 1, 0, 0 },
+                    { 1, 1, 1, 1 } };
+
+     solveMaze(maze);
+  }
+  public static void printSudoko(int sudoko[][]) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudoko[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printSolution(int sol[][]) {
+        for (int i = 0; i < sol.length; i++) {
+            for (int j = 0; j < sol.length; j++) {
+                System.out.print(" " + sol[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isSafe(int maze[][], int x, int y) {
+        // if (x, y outside maze) retrun false
+        return (x >= 0 && x < maze.length && y >= 0 && y < maze.length && maze[x][y] == 1);
+    }
+
+    public static boolean solveMaze(int maze[][]) {
+        int N = maze.length;
+        int sol[][] = new int[N][N];
+
+        if (solveMazeUtil(maze, 0, 0, sol) == false) {
+            System.out.print("Sollution does not exists");
+            return false;
+        }
+        printSolution(sol);
+        return true;
+    }
+
+    public static boolean solveMazeUtil(int maze[][], int x, int y, int sol[][]) {
+        int N = maze.length;
+        if (x == N - 1 && y == N - 1 && maze[x][y] == 1) {
+            sol[x][y] = 1;
+            ;
+            ;
+            return true;
+        }
+
+        // Check if maze[x][y] is valid
+        if (isSafe(maze, x, y) == true) {
+            if (sol[x][y] == 1) {
+                return false;
+            }
+            sol[x][y] = 1;
+            if (solveMazeUtil(maze, x + 1, y, sol)) {
+                return true;
+            }
+            if (solveMazeUtil(maze, x, y + 1, sol)) {
+                return true;
+            }
+            sol[x][y] = 0;
+            return false;
+        }
+        return false;
+    }
+
+}
