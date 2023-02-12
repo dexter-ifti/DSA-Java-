18c64d72ab722bfa4d607f974e6ce21b3680322f
Taha <tiftikhar@student.iul.ac.in>
2023-02-12 23:57:19 +0530

    Create Reverse.java

---
+++ 11_ArrayList/Reverse.java
@@ -0,0 +1,19 @@
+public class Revese
+{
+  public static void main(String args[])
+  {
+    ArrayList <Integer> list = new ArrayList<>();
+     list.add(1);
+     list.add(2);
+     list.add(3);
+     list.add(4);
+     list.add(5);
+
+     System.out.println(list);
+    
+     for (int i = list.size() - 1; i >= 0; i--) {
+          System.out.print(list.get(i) + " ");
+     }
+     System.out.println();
+ }
+}
