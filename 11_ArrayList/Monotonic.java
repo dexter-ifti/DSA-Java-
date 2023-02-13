932dcdb27402e04abf84275569360d2dfd784bc9
Taha <tiftikhar@student.iul.ac.in>
2023-02-14 00:28:21 +0530

    Create Monotonic.java

---
+++ 11_ArrayList/Monotonic.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Monotonic {
+    public static boolean isMonotonic(ArrayList<Integer>list) {
+        boolean inc = true; 
+        boolean dec = true ;
+
+        for(int i = 0 ; i < list.size()-1 ; i++){
+            if(list.get(i) > list.get(i+1)){
+                inc = false ;
+            }
+            if(list.get(i) < list.get(i+1)){
+                dec = false ;
+            }
+        }
+        return inc || dec ;
+        
+    }
+    public static void main(String[] args) {
+
+        ArrayList<Integer>list = new ArrayList<>();
+
+        list.add(1);
+        list.add(3);
+        list.add(6);
+
+        ArrayList<Integer>list1 = new ArrayList<>();
+        list1.add(6);
+        list1.add(8);
+        list1.add(2);
+
+        System.out.println(isMonotonic(list)); 
+        System.out.println(isMonotonic(list1));  
+    }
+}
