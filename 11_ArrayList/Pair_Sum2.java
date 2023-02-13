bc4277e5ebace365cf2c9ed4ffb9417df9a79ee2
Taha <tiftikhar@student.iul.ac.in>
2023-02-13 22:06:49 +0530

    Create Pair_Sum2.java

---
+++ 11_ArrayList/Pair_Sum2.java
@@ -0,0 +1,40 @@
+public class Pair_Sum2 {
+   public static void main(String args[]) {
+      ArrayList <Integer> list = new ArrayList<>();
+            list.add(11);
+            list.add(15);
+            list.add(6);
+            list.add(8);
+            list.add(9);
+            list.add(10);
+            int target = 16;
+            System.out.println(pairSum2(list, target));
+
+  }
+   public static boolean pairSum2(ArrayList <Integer> list, int target) {
+        int bp = -1;
+        int n = list.size();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > list.get(i+1)) {
+                bp = i;
+                break;
+            }
+        }
+
+        int lp = bp+1;
+        int rp = bp ;
+
+        while(lp != rp) {
+            //case 1
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            } if (list.get(lp) + list.get(rp) < target) {
+                lp = (lp + 1) % n;
+            }
+            else {
+                rp = (n + rp - 1) % n;
+            } 
+        }
+        return false;
+    }
+}
