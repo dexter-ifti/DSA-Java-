5a407b68ad869d1d2cb465bfcde19a7fa9b3ed6e
Taha <tiftikhar@student.iul.ac.in>
2023-02-17 23:13:00 +0530

    Update LL.java

--- 12_LinkedList/LL.java
+++ 12_LinkedList/LL.java
@@ -104,6 +104,38 @@ public class LL {
         size--;
         return val;
     }
+      public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;;
+        }
+        return -1;
+    }
+
+    public int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+
+        return idx+1;
+    }
+
+    public int recSearch(int key) {
+       return helper(head, key);
+    }
 
     public static void main(String[] args) {
         LL ll = new LL();
