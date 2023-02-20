c5c5f4835e8744183123a1608374870fe8417e0f
Taha <tiftikhar@student.iul.ac.in>
2023-02-20 22:33:47 +0530

    Create CycleLL.java

---
+++ 12_LinkedList/CycleLL.java
@@ -0,0 +1,77 @@
+import java.util.LinkedList;
+public class CycleLL {
+    public static class Node 
+    {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }    
+    }
+    
+    public static Node head;
+    public static Node tail;
+
+    public static boolean isCycle() {  // floyd's Algorithm
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void removeCycle() {
+        // detect Cycle
+        Node slow = head;
+        Node fast = head;
+
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+        if (!cycle) {
+            return;
+        }
+
+        // find meeting point 
+        slow = head;
+        Node prev = null; // last node
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // remove Cycle
+
+        prev.next = null;
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        Node temp  = new Node(2);
+        head.next  = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+
+        System.out.println(isCycle());
+        removeCycle();
+        System.out.println(isCycle());
+    }
+}
