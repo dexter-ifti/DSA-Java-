349187590ebaa76f64a73b1e9ab896c0b7f3977a
Taha <tiftikhar@student.iul.ac.in>
2023-02-25 00:39:33 +0530

    Create ReverseStack.java

---
+++ 13_Stacks/ReverseStack.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class ReverseStack {
+
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void reverse(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void printStack(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        reverse(s);
+        printStack(s);
+    }
+}
