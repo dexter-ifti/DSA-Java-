2ec3c3952263c562d03caad123ba1147fb524f1c
Taha <tiftikhar@student.iul.ac.in>
2023-02-25 00:24:27 +0530

    Create StringReverse.java

---
+++ 13_Stacks/StringReverse.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class StringReverse {
+
+  public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0 ;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);;
+        }
+
+        return result.toString();
+    }
+
+
+
+    public static void main(String[] args) {
+       String str = "abc";
+       String result = reverseString(str);
+       System.out.println(result);
+    }
+}
