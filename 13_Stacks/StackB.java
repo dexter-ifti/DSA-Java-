97f973c00b33550961dc97b60561d1fa606f4408
Taha <tiftikhar@student.iul.ac.in>
2023-02-25 19:51:52 +0530

    Add files via upload

---
+++ 13_Stacks/StackB.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class StackB {
+
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void reverse(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void printStack(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
+    public static void stockSpan(int stocks[], int span[]) {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for (int i = 1; i < stocks.length; i++) {
+            int currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                span[i] = i+1;
+            } else {
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh; 
+            }
+
+            s.push(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int stocks[] = {100, 80, 60, 70, 60, 85, 100};
+        int span[] = new int[stocks.length];
+        stockSpan(stocks, span);
+
+        for (int i = 0; i < span.length; i++) {
+            System.out.println(span[i]+" ");
+        }
+    }
+}
