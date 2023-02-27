2d0bec641d1f624f278c9e0ce2f73d53b57132e8
Taha <tiftikhar@student.iul.ac.in>
2023-02-27 10:30:47 +0530

    Create TrappingRainWater.java

---
+++ 1_Arrays/TrappingRainWater.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class TrappingRainWater {
+   
+    public static int trapWater(int height[]) {
+        int n = height.length;
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        }
+
+        for (int i = n-2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+        }
+
+        int water = 0;
+        for (int i = 0; i < n; i++) {
+            water += (Math.min(leftMax[i], rightMax[i])) - height[i];
+        }
+
+        return water;
+    }
+
+    public static void main(String args[]) {
+        int arrr[] = {4,2,0,3,2,5};
+        System.out.println("Max water = " + trapWater(arrr));
+    }
+}
