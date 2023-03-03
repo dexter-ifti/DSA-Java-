f2ea154e028a5fd55b03a304babe1bf912c0a5c5
Taha <tiftikhar@student.iul.ac.in>
2023-03-04 00:58:56 +0530

    Create Stack_Using_Deque.java

---
+++ 14_Queues/Stack_Using_Deque.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Stack_Using_Deque {
+
+    static class Stack {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void push(int data) {
+            deque.addLast(data);
+        }
+
+        public int pop() {
+            return deque.removeLast();
+        }
+
+        public int peek() {
+            return deque.getLast();
+        }
+    }
+   
+    public static void main(String[] args) {
+       Stack s = new Stack();
+       s.push(1);
+       s.push(2);
+       s.push(3);
+       s.push(4);
+       System.out.println("Peek = " + s.peek());
+       System.out.println(s.pop());
+       System.out.println(s.pop());
+       System.out.println(s.pop());
+       System.out.println(s.pop());
+    }
+}
