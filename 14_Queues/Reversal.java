e3b5d2cee53d77adf015c781f7ac2c2c9c668980
Taha <tiftikhar@student.iul.ac.in>
2023-03-03 23:28:55 +0530

    Create Reversal.java

---
+++ 14_Queues/Reversal.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class QueueB {
+
+    public static void reverse(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+
+        reverse(q);
+        while (!q.isEmpty()) {
+            System.out.print(q.remove()+ " ");
+        }
+        System.out.println();
+    }
+}
