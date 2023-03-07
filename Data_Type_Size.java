a727a96b23e7232c18ef4748262ba92d3ac79811
Taha <tiftikhar@student.iul.ac.in>
2023-03-07 22:52:37 +0530

    Create Data_Type_Size.java

---
+++ Data_Type_Size.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class QueueB {
+
+    public static void main(String[] args) {
+        System.out.println("Size of int: " + (Integer.SIZE / 8) + " bytes.");
+        System.out.println("Size of long: " + (Long.SIZE / 8) + " bytes.");
+        System.out.println("Size of char: " + (Character.SIZE / 8) + " bytes.");
+        System.out.println("Size of float: " + (Float.SIZE / 8) + " bytes.");
+        System.out.println("Size of double: " + (Double.SIZE / 8) + " bytes.");
+    }
+}
