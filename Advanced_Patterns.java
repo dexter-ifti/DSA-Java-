1c705f72d55c201f8922375eb516ced9294f6299
Taha <tiftikhar@student.iul.ac.in>
2023-03-09 21:53:10 +0530

    Update Advanced_Patterns.java

--- Advanced_Patterns.java
+++ Advanced_Patterns.java
@@ -38,4 +38,31 @@ public class Advanced_Patterns {
             System.out.println();
         }
     }
+    public static void butterfly_pattern (int n){
+        for (int i = 1 ; i<= n ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            for (int j = 1 ; j <= 2*(n-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n ; i>= 1 ; i--){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            for (int j = 1 ; j <= 2*(n-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
 }
