ea913f9d966f6fbb29f98c4b6918abdee90bcb7e
Taha <tiftikhar@student.iul.ac.in>
2023-03-10 00:09:39 +0530

    Update Reverse_K_Elements.java

--- 14_Queues/Reverse_K_Elements.java
+++ 14_Queues/Reverse_K_Elements.java
@@ -47,3 +47,50 @@ public class Reverse_K_Elements {
         System.out.println();
     }
 }
+
+// Meethod 2
+class Reverse_k_element_queue {
+ 
+ static Queue<Integer> queue;
+ static void reverseQueueFirstKElements(int k)
+  {
+    if (queue.isEmpty() == true
+      || k > queue.size())
+      return;
+    if (k <= 0)
+      return;
+    Stack<Integer> stack = new Stack<Integer>();
+    for (int i = 0; i < k; i++) {
+      stack.push(queue.peek());
+      queue.remove();
+    }
+    while (!stack.empty()) {
+      queue.add(stack.peek());
+      stack.pop();
+    }
+    for (int i = 0; i < queue.size() - k; i++) {
+      queue.add(queue.peek());
+      queue.remove();
+    }
+  }
+  static void Print()
+  {
+    while (!queue.isEmpty()) {
+      System.out.print(queue.peek() + " ");
+      queue.remove();
+    }
+  }
+  public static void main(String args[])
+  {
+    queue = new LinkedList<Integer>();
+    queue.add(5);
+    queue.add(10);
+    queue.add(15);
+    queue.add(20);
+    queue.add(25);
+    
+    int k = 3;
+    reverseQueueFirstKElements(k);
+    Print();
+  }
+}
