6bf6bb3df514f82dd9d53dd13a9bd6688c5a8a8c
Taha <tiftikhar@student.iul.ac.in>
2023-04-06 23:11:48 +0530

    Add files via upload

---
+++ 17_BinarySearchTree/BST2.java
@@ -0,0 +1,41 @@
+public class BST2 {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static Node createBST(int arr[], int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        Node root = new Node(arr[mid]);
+        root.left = createBST(arr, start, mid - 1);
+        root.right = createBST(arr, mid + 1, end);
+
+        return root;
+    }
+    
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 5, 6, 8, 10, 11, 13 };
+        Node root = createBST(arr, 0, arr.length - 1);
+        preorder(root);
+    }
+}
