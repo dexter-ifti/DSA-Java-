e94b12f705cfec0b000a7c26e4672f3e94a61a8d
Taha <tiftikhar@student.iul.ac.in>
2023-07-28 00:03:43 +0530

    Create Merge_two_Sorted_List.java

---
+++ 00_Problems /LinkedList/Merge_two_Sorted_List.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+// https://practice.geeksforgeeks.org/problems/merge-two-sorted-linked-lists/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummyhead = new ListNode();
+        ListNode tail = dummyhead;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            }else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyhead.next;
+    }
+}
+/*
+21. Merge Two Sorted List
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+*/
