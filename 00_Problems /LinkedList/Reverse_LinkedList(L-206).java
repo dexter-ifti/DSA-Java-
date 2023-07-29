719be0ca02fd6cb70700813cf81e4db0ad04fcad
Taha <tiftikhar@student.iul.ac.in>
2023-07-29 09:09:06 +0530

    Create Reverse_LinkedList(L-206).java

---
+++ 00_Problems /LinkedList/Reverse_LinkedList(L-206).java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/reverse-linked-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while(present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+}
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
