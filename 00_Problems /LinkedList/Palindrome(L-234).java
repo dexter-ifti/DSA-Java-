2dd12b867b49d503c1b230472d9bbf3ae349835f
Taha <tiftikhar@student.iul.ac.in>
2023-07-29 11:49:44 +0530

    Create Palindrome(L-234).java

---
+++ 00_Problems /LinkedList/Palindrome(L-234).java
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/palindrome-linked-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head){
+        ListNode mid = middleListNode(head);
+        ListNode headSecond = reverseList(mid);
+        ListNode reverseHead = headSecond;
+
+        while (head != null && headSecond != null) {
+            if(head.val != headSecond.val){
+                break;
+            }
+            head = head.next;
+            headSecond = headSecond.next;
+        }
+        reverseList(reverseHead);
+        return head == null || headSecond == null;
+    }
+    public ListNode middleListNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
+    public ListNode reverseList(ListNode head){
+        if(head == null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while(present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+}
+/*Given the head of a singly linked list, return true if it is a 
+palindrome
+ or false otherwise.
+*/
