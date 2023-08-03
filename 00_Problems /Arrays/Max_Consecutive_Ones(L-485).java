c339ec67b027240dc38e3fdbc692acc869b8f171
Taha <tiftikhar@student.iul.ac.in>
2023-08-03 18:29:05 +0530

    Create Max_Consecutive_Ones(L-485).java

---
+++ 00_Problems /Arrays/Max_Consecutive_Ones(L-485).java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+       int count = 0;
+       int max = 0;
+       for(int i = 0; i < nums.length; i++){
+           if(nums[i] == 1){
+               count++;
+           }else{
+               count = 0;
+           }
+       max = Math.max(count, max);
+       }
+       return max;
+    }
+}
