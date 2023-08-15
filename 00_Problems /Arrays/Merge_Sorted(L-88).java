1ca8898a1b8b56257c035ae45e806777ad6d9a67
Taha <tiftikhar@student.iul.ac.in>
2023-08-15 18:49:14 +0530

    Create Merge_Sorted(L-88).java

---
+++ 00_Problems /Arrays/Merge_Sorted(L-88).java
@@ -0,0 +1,25 @@
+// O((m+n)log(m+n))
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = m, j = 0; j < n; j++){
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+// O(m+n)
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(j >= 0){
+            if(i >= 0 && nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+}
