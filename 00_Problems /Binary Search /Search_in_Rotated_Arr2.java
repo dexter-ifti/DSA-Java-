45982d42e1beb8dd5d678d5d6f29567bb6c9c5ba
Taha <tiftikhar@student.iul.ac.in>
2023-08-18 10:28:17 +0530

    Create Search_in_Rotated_Arr2.java

---
+++ 00_Problems /Binary Search /Search_in_Rotated_Arr2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int s = 0; 
+        int e = nums.length - 1;
+
+        while(s <= e){
+            int m = s + (e - s)/2;
+            if(nums[m] == target){
+                return true;
+            }
+             if (nums[s] == nums[m] && nums[m] == nums[e]) {
+                s = s + 1;
+                e = e - 1;
+                continue;
+            }
+            if(nums[s] <= nums[m]){
+                if(target >= nums[s] && target <= nums[m]){
+                    e = m - 1;
+                } else{
+                    s = m + 1;
+                }
+            } else {
+                if(target >= nums[m] && target <= nums[e]){
+                    s = m + 1;
+                }else {
+                    e = m - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
