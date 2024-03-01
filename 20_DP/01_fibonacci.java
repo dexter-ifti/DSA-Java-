8b2b06875b04e4d9c7fa26d8b601091efaf16a1d
Taha <tiftikhar@student.iul.ac.in>
2024-03-01 16:22:27 +0530

    edge case handled

--- 20_DP/01_fibonacci.java
+++ 20_DP/01_fibonacci.java
@@ -18,7 +18,7 @@ int fibonacci(int n, int[] dp){
 // tabulation TC - O(n), SC - O(n)
 int fibonacci_1(int n, int[] dp){
     dp[0] = 0; 
-    dp[1] = 1;
+    if(n > 0) dp[1] = 1;
     if(n <= 1) return dp[n];
     for(int i = 2; i <= n; i++){
         dp[i] = dp[i-1] + dp[i-2];
